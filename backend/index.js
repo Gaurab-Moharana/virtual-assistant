06401ff7ade111f35a4da9d18c5375f53f02e222
Gaurab-Moharana <151185962+Gaurab-Moharana@users.noreply.github.com>
2025-06-22 11:51:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-rlhf.onrender.com",
     credentials: true,
   })
 );
